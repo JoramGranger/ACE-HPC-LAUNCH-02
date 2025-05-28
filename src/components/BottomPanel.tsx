a2f4c1164528a4fab6b37d26ce93dedd40e871c6
joram.andrew.k@gmail.com <joram.andrew.k@gmail.com>
2025-05-29 00:54:04 +0300

    logod

--- src/components/BottomPanel.tsx
+++ src/components/BottomPanel.tsx
@@ -1,43 +1,72 @@
 import { useState, useEffect } from "react";
 import LogoPanel from "./LogoPanel";
 
+// Collaboration logos (already imported)
+import aceLogo from '../assets/ace-logo.png';
+import idiLogo from '../assets/idi-logo.png';
+import makLogo from '../assets/makerere-university-logo.jpg';
+import renuLogo from '../assets/renu-logo.png';
+import rdctLogo from '../assets/rdct-logo.jpeg';
+import taccLogo from '../assets/tacc-logo.jpg';
+
+// Technology stack logos
+import anancondaLogo from '../assets/ananconda.png';
+import rockyLogo from '../assets/rocky-logo.png';
+import ubuntuLogo from '../assets/ubuntu-logo.png';
+import openhpcLogo from '../assets/openhpc.jpg';
+import slurmLogo from '../assets/Slurm_logo.svg';
+import freeipaLogo from '../assets/freeIPA.png';
+import prometheusLogo from '../assets/prometheus-logo.png';
+import grafanaLogo from '../assets/grafana-logo.png';
+
+// Hardware partner logos
+import intelLogo from '../assets/intel-logo.png';
+import ciscoLogo from '../assets/cisco-logo.png';
+import dellLogo from '../assets/dell-logo.png';
+import netappLogo from '../assets/netapp.png';
+import veeamLogo from '../assets/veeam-logo.jpg';
+import hpeLogo from '../assets/HPE.png';
+import synologyLogo from '../assets/synology.png';
+import apcLogo from '../assets/apc-logo.png';
+
 const BottomPanel = () => {
   const [currentPanel, setCurrentPanel] = useState(0);
 
   const technologyLogos = [
-    { name: "Rocky Linux", url: "https://rockylinux.org/images/logo.svg" },
-    { name: "Ubuntu", url: "https://assets.ubuntu.com/v1/29985a98-ubuntu-logo32.png" },
-    { name: "OpenHPC", url: "https://openhpc.community/wp-content/uploads/2017/05/openhpc_logo.png" },
-    { name: "SLURM", url: "https://slurm.schedmd.com/slurm_logo.png" },
-    { name: "Anaconda", url: "https://www.anaconda.com/assets/images/anaconda-logo.svg" },
-    { name: "FreeIPA", url: "https://www.freeipa.org/images/freeipa/freeipa-logo-small.png" },
-    { name: "Prometheus", url: "https://prometheus.io/assets/prometheus_logo_grey.svg" },
-    { name: "Grafana", url: "https://grafana.com/static/img/logos/grafana_logo-web_white.svg" }
+    { name: "Rocky Linux", url: rockyLogo },
+    { name: "Ubuntu", url: ubuntuLogo },
+    { name: "OpenHPC", url: openhpcLogo },
+    { name: "SLURM", url: slurmLogo },
+    { name: "Anaconda", url: anancondaLogo },
+    { name: "FreeIPA", url: freeipaLogo },
+    { name: "Prometheus", url: prometheusLogo },
+    { name: "Grafana", url: grafanaLogo }
   ];
 
   const hardwareLogos = [
-    { name: "Intel", url: "https://www.intel.com/content/dam/logos/intel-header-logo-white.svg" },
-    { name: "Cisco", url: "https://www.cisco.com/c/dam/en/us/td/i/200001-300000/220001-230000/229001-230000/229410.jpg" },
-    { name: "Dell", url: "https://www.dell.com/images/global/brand/ui/logo-wt-bl.png" },
-    { name: "NetApp", url: "https://www.netapp.com/img/netapp-logo-white.svg" },
-    { name: "Veeam", url: "https://www.veeam.com/content/dam/veeam/global/veeam-graphics/veeam_logo_white.svg" },
-    { name: "HPE", url: "https://www.hpe.com/etc.clientlibs/hpe/clientlibs/clientlib-hpe-static/resources/images/hpe-logo-white.svg" },
-    { name: "Synology", url: "https://www.synology.com/img/company/logo_white.svg" },
-    { name: "APC", url: "https://www.apc.com/us/en/assets/content/apc-logo-white.svg" }
+    { name: "Intel", url: intelLogo },
+    { name: "Cisco", url: ciscoLogo },
+    { name: "Dell", url: dellLogo },
+    { name: "NetApp", url: netappLogo },
+    { name: "Veeam", url: veeamLogo },
+    { name: "HPE", url: hpeLogo },
+    { name: "Synology", url: synologyLogo },
+    { name: "APC", url: apcLogo }
   ];
 
   const collaborationLogos = [
-    { name: "ACE", url: "https://example.com/ace-logo.png" },
-    { name: "IDI", url: "https://example.com/idi-logo.png" },
-    { name: "RENU", url: "https://example.com/renu-logo.png" },
-    { name: "RDCT", url: "https://example.com/rdct-logo.png" },
-    { name: "TACC", url: "https://www.tacc.utexas.edu/images/tacc-logo-white.svg" }
+    { name: 'ACE', url: aceLogo },
+    { name: "IDI", url: idiLogo },
+    { name: "MAK", url: makLogo },
+    { name: "RENU", url: renuLogo },
+    { name: "TACC", url: taccLogo },
+    { name: "RDCT", url: rdctLogo }
   ];
 
   const panels = [
-    { title: "Technology Stack", logos: technologyLogos, label: "built with" },
-    { title: "Hardware Partners", logos: hardwareLogos, label: "built on" },
-    { title: "Collaborators", logos: collaborationLogos, label: "built by" }
+    { title: "Technology Stack", logos: technologyLogos, label: "Built With" },
+    { title: "Hardware Partners", logos: hardwareLogos, label: "Built On" },
+    { title: "Collaborators", logos: collaborationLogos, label: "In Collaboration With" }
   ];
 
   useEffect(() => {
